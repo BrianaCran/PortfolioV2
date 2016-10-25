2fd95c54ecf60a0813376d8c188e5f3b5c3b3cd7
BrianaCran <briana.marie.crandall@gmail.com>
2016-10-24 22:48:33 -0400

    javascript implementation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 
-function myFunction(){
+function ptfg(){
                 document.getElementById('ptfg').style.display = "block";
+                document.getElementById('ptfgDetails').style.display = "block";
+                document.getElementById('goodreads').style.display = "none";
+};
+function goodreads(){
+                document.getElementById('goodreads').style.display = "block";
+                document.getElementById('ptfg').style.display = "none";
+                document.getElementById('ptfgDetails').style.display = "none";
+
 };
