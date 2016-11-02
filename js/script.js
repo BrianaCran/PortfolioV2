ae113663c359b4a99ff8c8c9d698e37292ec6a30
BrianaCran <briana.marie.crandall@gmail.com>
2016-11-02 14:19:02 -0400

    images update, project section update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 
 function ptfg(){
                 document.getElementById('ptfg').style.display = "block";
-                document.getElementById('ptfgDetails').style.display = "block";
+                document.getElementById('ptfgDetails').style.display = "inline-block";
                 document.getElementById('goodreads').style.display = "none";
 };
 function goodreads(){
                 document.getElementById('goodreads').style.display = "block";
+                document.getElementById('grDetails').style.display = "inline-block";
                 document.getElementById('ptfg').style.display = "none";
                 document.getElementById('ptfgDetails').style.display = "none";
 
